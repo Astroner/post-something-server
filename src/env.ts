649fc17bf1b857ce8a4070cf747a56a42d8c00fd
Astroner <zhenyanikforov@gmail.com>
2020-09-10 22:08:48 +0300

    envs

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,6 @@
 export default {
-	PORT: +process.env.PORT || 80,
-	HOST: process.env.HOST || 'localhost',
-	SECRET: process.env.SECRET || 'ASDljhk1j2hkjahskdjk1jh2k3haaaa',
-	MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost/post',
+	PORT: +process.env.PORT,
+	HOST: process.env.HOST,
+	SECRET: process.env.SECRET,
+	MONGO_URL: process.env.MONGO_URL,
 };
